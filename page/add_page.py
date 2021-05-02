c8bd2b571ddd256e11316d524f39c4c75e13f9bc
不爱吃鱼的猫丶 <1716224950@qq.com>
2021-05-02 20:57:01 +0800

    完成实验二

--- page/add_page.py
+++ page/add_page.py
@@ -8,6 +8,13 @@ class AddPage(Page):
     forum_option = Element(id_='setforum', describe="设置板块选项")
     class_input = Element(xpath='/html/body/div[1]/table/tbody/tr[2]/td/form/input[3]', describe="新分类输入框")
     class_submit_button = Element(xpath='/html/body/div[1]/table/tbody/tr[2]/td/form/input[4]', describe="新分类提交按钮")
+    submit_button2 = Element(xpath='/html/body/div[1]/form/center/input[1]', describe="提交按钮2")
+    confirm_button = Element(xpath='/html/body/div[1]/div/table/tbody/tr/td[2]/form/span/input[2]', describe="确认提交")
+
     forum_input = Element(xpath='//*[@id="forum_form"]/input[4]', describe="板块输入框")
-    forum_submit_button = Element(xpath='//*[@id="forum_form"]/input[6]', describe="板块提交按钮")
     parent_forum_selector = Element(xpath='//*[@id="forum_form"]/select', describe="父板块单选框")
+    forum_submit_button = Element(xpath='//*[@id="forum_form"]/input[6]', describe="板块提交按钮")
+    forum_submit_button2 = Element(xpath='/html/body/div[1]/form/center/input[1]', describe="板块提交按钮2")
+    forum_confirm_submit = Element(xpath='/html/body/div[1]/div/table/tbody/tr/td[2]/form/span/input[2]',
+                                   describe="板块确认提交")
+    forum_index = Element(xpath='/html/body/div[1]/ul/li[1]/a', describe="板块管理主页")
--- test_dir/test_add.py
+++ test_dir/test_add.py
@@ -1,30 +1,76 @@
 import sys
-from time import sleep
-
+import time
+import unittest
 import ddt
-import pytest
+import HTMLTestRunner
 from os.path import dirname, abspath
 
+import yagmail
+from selenium import webdriver
+from selenium.webdriver.support.select import Select
 from page.add_page import AddPage
 
 sys.path.insert(0, dirname(dirname(abspath(__file__))))
 
 
 @ddt.ddt
-class TestAdd:
-    """添加分类测试"""
-
-    @ddt.file_data("F:/program/PythonWorkSpace/selenium/exp/exp_unittest/data/test_add_type.json")
-    def test_add_class(self, browser, base_url):
-        """
+class TestAdd(unittest.TestCase):
+    """
        以管理员身份登录，进入后台页面，实现添加一批新分类和新板块的功能。
        需要添加的一批新分类的数据﹑新板块数据都放在json文件中.
        生成html格式的report并发送到指定邮箱
-        """
-        page = AddPage(browser)
-        page.get(base_url)
+    """
+
+    @classmethod
+    def setUpClass(cls):
+        cls.driver = webdriver.Chrome()
+        cls.driver.maximize_window()
+        cls.driver.get("http://localhost/phpwind/admin.php")
+        page = AddPage(cls.driver)
         page.usr_input = "admin"
         page.pwd_input = "123456"
         page.login_button.click()
         page.forum_option.click()
-        browser.switch_to.frame('main')
+        cls.driver.switch_to.frame('main')
+        cls.page = page
+
+    @ddt.file_data("F:/PyProjects/pyAutoTest/test_dir/data/data_class.json")
+    @ddt.unpack  # 每次运行都会从 data 中取出一组数据，动态生成一个独立的测试用例方法
+    def test1_add_class(self, classname):
+        u"""添加分类"""
+        self.page.class_input = classname
+        self.page.class_submit_button.click()
+        self.page.submit_button2.click()
+        self.page.confirm_button.click()
+        self.assertIn(classname, self.driver.page_source)
+
+    @ddt.file_data("F:/PyProjects/pyAutoTest/test_dir/data/data_forum.json")
+    @ddt.unpack  # 每次运行都会从 data 中取出一组数据，动态生成一个独立的测试用例方法
+    def test2_add_forum(self, forum_name, parent_class):
+        u"""添加板块"""
+        self.page.forum_input = forum_name
+        s = self.driver.find_element_by_xpath('//*[@id="forum_form"]/select')
+        Select(s).select_by_visible_text(">> " + parent_class)
+        self.page.forum_submit_button.click()  # 提交按钮
+        self.page.forum_submit_button2.click()  # 第二个提交按钮
+        self.page.forum_confirm_submit.click()  # 确认提交
+        self.page.forum_index.click()  # 返回板块管理主页
+        self.assertIn(forum_name, self.driver.page_source)
+
+
+def send_mail(report):
+    yag = yagmail.SMTP(user="1716224950@qq.com", password="tydqvqptzqobbjji", host="smtp.qq.com")
+    subject = "自动化测试报告"
+    contents = "测试报告请查看附件"
+    yag.send("1716224950@qq.com", subject, contents, report)
+    print("email has send out!")
+
+
+if __name__ == '__main__':
+    report_html = "F:\\PyProjects\\pyAutoTest\\test_report\\" + time.strftime("%Y_%m_%d_%H_%M_%S_result.html")
+    fp = open(report_html, "wb")
+    suit = unittest.defaultTestLoader.discover(start_dir="F:\\PyProjects\\pyAutoTest\\test_dir", pattern="test_add.py")
+    runner = HTMLTestRunner.HTMLTestRunner(stream=fp, title=u"软件测试报告", description=u"用例执行过程")
+    runner.run(suit)
+    fp.close()
+    send_mail(report_html)
