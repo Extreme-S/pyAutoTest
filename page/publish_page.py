2fb4acb636652e453e7267e14109990a578d8ba1
不爱吃鱼的猫丶 <1716224950@qq.com>
2021-05-06 23:30:00 +0800

    完成publish_page.py配置

---
+++ page/publish_page.py
@@ -0,0 +1,17 @@
+from poium import Page, Element
+
+
+class PublishPage(Page):
+    clear_cookie = Element(xpath='//*[@id="mode-footer"]/div/div[4]/ul/li[1]/a', describe="清除cookie")
+    login_option = Element(xpath='//*[@id="nav-user"]/table/tbody/tr/td/a[1]', describe="登录选项")
+    usr_input = Element(xpath='//*[@id="main"]/form/div/table/tbody/tr[2]/td/div/dl[1]/dd/input',
+                        describe="username输入框")
+    pwd_input = Element(xpath='//*[@id="main"]/form/div/table/tbody/tr[2]/td/div/dl[2]/dd/input', describe="pwd输入框")
+    login_button = Element(xpath='//*[@id="main"]/form/div/table/tbody/tr[2]/td/div/dl[5]/dd/input', describe="登录按钮")
+
+    index_page = Element(xpath='//*[@id="nav-global"]/li[1]/a', describe="首页")
+    publish_option = Element(xpath='//*[@id="td_post"]', describe="发表新帖选项")
+    title_input = Element(xpath='//*[@id="atc_title"]', describe="新帖标题输入")
+    content_input = Element(xpath='/html/body', describe="新帖内容输入")
+    commit_button = Element(xpath='//*[@id="main"]/form[1]/div/table[2]/tbody/tr[4]/td[1]/div/input[1]',
+                            describe="新帖提交")
---
+++ test_dir/test_publish.py
@@ -0,0 +1,32 @@
+import sys
+import time
+import unittest
+import ddt
+import HTMLTestRunner
+from os.path import dirname, abspath
+
+import yagmail
+from selenium import webdriver
+from selenium.webdriver.support.select import Select
+from page.add_page import AddPage
+from page.publish_page import PublishPage
+
+sys.path.insert(0, dirname(dirname(abspath(__file__))))
+
+
+class TestPublish:
+    """论坛发帖功能的测试"""
+
+    @classmethod
+    def setup_class(cls, browser, base_url):
+        page = PublishPage(browser)
+        cls.page = page
+        page.get(base_url)
+        page.clear_cookie.click()
+        page.login_option.click()
+        page.usr_input = "admin"
+        page.pwd_input = "123456"
+        page.login_button.click()
+
+    def test_publish(self):
+        page = self.page
